5886a82fadd3483a904e7055009ed07763c1ef7e
opencookiecms <syed.m.afiq@outlook.com>
2018-08-28 00:06:50 +0800

    adding some comment

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -6,6 +6,15 @@ class Home extends CI_Controller{
 
   public function index()
   {
+    // load the template header,sidebar,pages and footer
+    // this is a Daftar page please refer to link http://localhost/jpspro
+    //
+    // kalau nak tengok template boleh pergi ke
+    // view->pages (ada page dekat sini)
+    //       template (Web template dekat sini)
+
+    //good luck
+
       $this->load->view('template/header');
       $this->load->view('template/sidebar');
       $this->load->view('pages/dashboard');
