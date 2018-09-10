073b81ef14598f8a1d9b6bd38fd23f5850e8f82e
opencookiecms <syed.m.afiq@outlook.com>
2018-09-11 01:34:06 +0800

    s

--- application/controllers/MRK.php
+++ application/controllers/MRK.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Classname extends CI_Controller{
+class MRK extends CI_Controller{
 
   public function __construct()
   {
