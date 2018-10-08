9b6d19b1b3189038b802ebf55acd5bb90d91613a
Alyya Izzati <33283754+alyyaizzati@users.noreply.github.com>
2018-10-08 10:39:22 +0800

    sql

--- assets/sql/jpspro.sql
+++ assets/sql/jpspro.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 27, 2018 at 09:20 AM
+-- Generation Time: Oct 08, 2018 at 04:37 AM
 -- Server version: 10.1.26-MariaDB
 -- PHP Version: 7.1.9
 
@@ -65,20 +65,27 @@ CREATE TABLE `dp_projek` (
   `df_nosebutharga` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   `df_tarikmohon` date DEFAULT NULL,
   `df_jsebutharga` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `df_tajuk` varchar(255) COLLATE utf8_bin DEFAULT NULL
+  `df_tajuk` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `df_daerah` varchar(25) COLLATE utf8_bin DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Dumping data for table `dp_projek`
 --
 
-INSERT INTO `dp_projek` (`id`, `df_nosebutharga`, `df_tarikmohon`, `df_jsebutharga`, `df_tajuk`) VALUES
-(1, 'BKK291/SH', '2018-09-07', 'wwwqd', 'lkoijio'),
-(2, 'BKK291/SH/B/001', '2018-09-19', 'Tetap', 'Menanam ubi'),
-(3, 'BKK291/SH/B/003', '2018-09-19', 'Sementara', 'Membersihkan rumah kucing'),
-(4, 'BKK291/SH/007', '2018-08-29', 'wwwqd', ''),
-(5, 'test', '2018-09-28', 'test', ''),
-(6, 'BKK291/SH', '2018-09-13', 'Tetap', '');
+INSERT INTO `dp_projek` (`id`, `df_nosebutharga`, `df_tarikmohon`, `df_jsebutharga`, `df_tajuk`, `df_daerah`) VALUES
+(1, 'BKK291/SH', '2018-09-07', 'wwwqd', 'lkoijio', NULL),
+(2, 'BKK291/SH/B/001', '2018-09-19', 'Tetap', 'Menanam ubi', NULL),
+(3, 'BKK291/SH/B/003', '2018-09-19', 'Sementara', 'Membersihkan rumah kucing', NULL),
+(4, 'BKK291/SH/007', '2018-08-29', 'wwwqd', '', NULL),
+(5, 'test', '2018-09-28', 'test', '', NULL),
+(6, 'BKK291/SH', '2018-09-13', 'Tetap', '', NULL),
+(7, 'BKK291/SH', '2018-10-04', 'Tetap', 's', NULL),
+(8, 'Dasdasd', '2018-10-10', 'Sebutharga', 'q', NULL),
+(9, 'BKK291/SH', '2018-10-11', 'Sebutharga', 'tttttttttttttttt', 'Baling'),
+(10, 'BKK291/SH', '2018-10-11', 'Sebutharga', 'asdaasd', 'Baling'),
+(11, 'tryrt', '2018-10-25', 'Lantikan Terus', 'tryr', 'Kuala Muda'),
+(12, 'BKK291/SH', '2018-10-04', 'Sebutharga', '33333333XFEF', 'Baling');
 
 -- --------------------------------------------------------
 
@@ -154,15 +161,16 @@ CREATE TABLE `mrk_dua` (
   `mrk_projekmansuh` date DEFAULT NULL,
   `mrk_pegawai` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   `mrk_jawatan` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `mrk_tarikhlaporan` date DEFAULT NULL
+  `mrk_tarikhlaporan` date DEFAULT NULL,
+  `mrkdua_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Dumping data for table `mrk_dua`
 --
 
-INSERT INTO `mrk_dua` (`id`, `mrk_nopkk`, `mrk_namakon`, `mrk_noinden`, `mrk_kosprojek`, `mrk_nokontrak`, `mrk_tarikhmulakon`, `mrk_tarikhjangkasiap`, `mrk_tajukkerja`, `mrk_majukerja`, `mrk_majukerjasebenar`, `mrk_bayarmajusemasa`, `mrk_jumlahbayarmaju`, `mrk_masalah`, `mrk_sebaboleh`, `mrk_lainlain`, `mrk_lanjutmasa`, `mrk_dari`, `mrk_sehingga`, `mrk_disebab`, `mrk_ladsehari`, `mrk_laddari`, `mrk_ladsehingga`, `mrk_perakukerjataksiap`, `mrk_projekmansuh`, `mrk_pegawai`, `mrk_jawatan`, `mrk_tarikhlaporan`) VALUES
-(1, 'a', '', '', '0', '', '0000-00-00', '0000-00-00', '', 0, '0000-00-00', 0, '0', 'modal', NULL, '', '', '0000-00-00', '0000-00-00', '', '0', '0000-00-00', '0000-00-00', '0000-00-00', '0000-00-00', '', '', '0000-00-00');
+INSERT INTO `mrk_dua` (`id`, `mrk_nopkk`, `mrk_namakon`, `mrk_noinden`, `mrk_kosprojek`, `mrk_nokontrak`, `mrk_tarikhmulakon`, `mrk_tarikhjangkasiap`, `mrk_tajukkerja`, `mrk_majukerja`, `mrk_majukerjasebenar`, `mrk_bayarmajusemasa`, `mrk_jumlahbayarmaju`, `mrk_masalah`, `mrk_sebaboleh`, `mrk_lainlain`, `mrk_lanjutmasa`, `mrk_dari`, `mrk_sehingga`, `mrk_disebab`, `mrk_ladsehari`, `mrk_laddari`, `mrk_ladsehingga`, `mrk_perakukerjataksiap`, `mrk_projekmansuh`, `mrk_pegawai`, `mrk_jawatan`, `mrk_tarikhlaporan`, `mrkdua_id`) VALUES
+(1, 'a', '', '', '0', '', '0000-00-00', '0000-00-00', '', 0, '0000-00-00', 0, '0', 'modal', NULL, '', '', '0000-00-00', '0000-00-00', '', '0', '0000-00-00', '0000-00-00', '0000-00-00', '0000-00-00', '', '', '0000-00-00', 0);
 
 -- --------------------------------------------------------
 
@@ -320,19 +328,21 @@ CREATE TABLE `mrk_satu` (
   `mrk_pegawai` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   `mrk_jawatan` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   `mrk_kosprojek` decimal(10,0) DEFAULT NULL,
-  `mrk_tarikh` date DEFAULT NULL
+  `mrk_tarikh` date DEFAULT NULL,
+  `mrksatu_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Dumping data for table `mrk_satu`
 --
 
-INSERT INTO `mrk_satu` (`id`, `mrk_nopkk`, `mrk_gred`, `mrk_namakon`, `mrk_alamatkon`, `mrk_nokontrak`, `mrk_noinden`, `mrk_tajukkerja`, `mrk_kategori`, `mrk_daerah`, `mrk_negeri`, `mrk_khusus`, `mrk_tarikhmulakon`, `mrk_tarikhjangkasiap`, `mrk_pegawai`, `mrk_jawatan`, `mrk_kosprojek`, `mrk_tarikh`) VALUES
-(1, 'a', 'g1', '', '', 'a', 'a', '', 'CE', 'kuala muda', 'kedah', 'CE 02', '0000-00-00', '0000-00-00', 'mr', 'mtk', '0', '0000-00-00'),
-(2, 'saas', 'g4', 'dsasd', '1488, Jalan Mutiara, 6/6, Taman Mutiara, 09700 Karangan Kedah', 'sdasda', 'sdsadas', 'dsadad', 'B', 'kuala muda', 'kedah', 'M 01', '2018-09-20', '2018-09-20', 'o', 'jb', '21', '2018-09-08'),
-(3, 'BSH/11', 'g2', 'Abu', '1488, Jalan Mutiara, 6/6, Taman Mutiara, 09700 Karangan Kedah', '12212', '2121212', 'Tanam Ubi', 'B', 'baling', 'kedah', 'CE 02', '2018-09-21', '2018-09-21', 'mr', 'mtk', '120', '2018-09-15'),
-(4, 'BSH/11/S', 'G2 | RM200,001.00 Hingga RM500,000.00', 'Abu', '1488, Jalan Mutiara, 6/6, Taman Mutiara, 09700 Karangan Kedah', '12212QWQE', 'QEQWE', 'E', 'ME | Mekanikal & Elektrikal', 'kuala muda', 'kedah', 'CE 14', '0000-00-00', '2018-09-13', 'Mohamad Ridzam B. Jusoh', 'Jurutera ( Kuala Muda/Sik )', '21', '2018-09-07'),
-(5, 'BSH/11/S/B', 'G3 | RM500,001.00 Hingga RM1 000,000.00', 'Alyya Izzati', 'Taman Delima', 'BSH/12/DLM/21', 'IM12213', 'Membina empangan', 'CE | Pembinaan Kejuruteraan Awam', 'kuala muda', 'kedah', 'CE 02', '2018-09-13', '2018-09-14', 'Mohamad Ridzam B. Jusoh', 'Jurutera ( Kuala Muda/Sik )', '12', '2018-09-14');
+INSERT INTO `mrk_satu` (`id`, `mrk_nopkk`, `mrk_gred`, `mrk_namakon`, `mrk_alamatkon`, `mrk_nokontrak`, `mrk_noinden`, `mrk_tajukkerja`, `mrk_kategori`, `mrk_daerah`, `mrk_negeri`, `mrk_khusus`, `mrk_tarikhmulakon`, `mrk_tarikhjangkasiap`, `mrk_pegawai`, `mrk_jawatan`, `mrk_kosprojek`, `mrk_tarikh`, `mrksatu_id`) VALUES
+(1, 'a', 'g1', '', '', 'a', 'a', '', 'CE', 'kuala muda', 'kedah', 'CE 02', '0000-00-00', '0000-00-00', 'mr', 'mtk', '0', '0000-00-00', 0),
+(2, 'saas', 'g4', 'dsasd', '1488, Jalan Mutiara, 6/6, Taman Mutiara, 09700 Karangan Kedah', 'sdasda', 'sdsadas', 'dsadad', 'B', 'kuala muda', 'kedah', 'M 01', '2018-09-20', '2018-09-20', 'o', 'jb', '21', '2018-09-08', 0),
+(3, 'BSH/11', 'g2', 'Abu', '1488, Jalan Mutiara, 6/6, Taman Mutiara, 09700 Karangan Kedah', '12212', '2121212', 'Tanam Ubi', 'B', 'baling', 'kedah', 'CE 02', '2018-09-21', '2018-09-21', 'mr', 'mtk', '120', '2018-09-15', 0),
+(4, 'BSH/11/S', 'G2 | RM200,001.00 Hingga RM500,000.00', 'Abu', '1488, Jalan Mutiara, 6/6, Taman Mutiara, 09700 Karangan Kedah', '12212QWQE', 'QEQWE', 'E', 'ME | Mekanikal & Elektrikal', 'kuala muda', 'kedah', 'CE 14', '0000-00-00', '2018-09-13', 'Mohamad Ridzam B. Jusoh', 'Jurutera ( Kuala Muda/Sik )', '21', '2018-09-07', 0),
+(5, 'BSH/11/S/B', 'G3 | RM500,001.00 Hingga RM1 000,000.00', 'Alyya Izzati', 'Taman Delima', 'BSH/12/DLM/21', 'IM12213', 'Membina empangan', 'CE | Pembinaan Kejuruteraan Awam', 'kuala muda', 'kedah', 'CE 02', '2018-09-13', '2018-09-14', 'Mohamad Ridzam B. Jusoh', 'Jurutera ( Kuala Muda/Sik )', '12', '2018-09-14', 0),
+(6, 'dffwe', 'G3 | RM500,001.00 Hingga RM1 000,000.00', 'ewfwefw', '1488, Jalan Mutiara, 6/6, Taman Mutiara, 09700 Karangan Kedah', 'wegwg', 'wegweg', 'ewgwg', 'CE | Pembinaan Kejuruteraan Awam', 'kuala muda', 'kedah', 'CE 02', '0000-00-00', '0000-00-00', 'Mohamad Ridzam B. Jusoh', 'Merinyu Taliair Kanan', '0', '0000-00-00', 0);
 
 -- --------------------------------------------------------
 
@@ -423,15 +433,16 @@ CREATE TABLE `mrk_tiga` (
   `mrk_kossebenar` decimal(10,0) DEFAULT NULL,
   `mrk_laddari` date DEFAULT NULL,
   `mrk_ladsehingga` date DEFAULT NULL,
-  `mrk_ladsehari` decimal(10,0) DEFAULT NULL
+  `mrk_ladsehari` decimal(10,0) DEFAULT NULL,
+  `mrktiga_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Dumping data for table `mrk_tiga`
 --
 
-INSERT INTO `mrk_tiga` (`id`, `mrk_nopkk`, `mrk_namakon`, `mrk_nokontrak`, `mrk_noinden`, `mrk_tajukkerja`, `mrk_tarikhmulakon`, `mrk_tarikhjangkasiap`, `mrk_lanjutmasa`, `mrk_siapsebenar`, `mrk_kosprojek`, `mrk_kossebenar`, `mrk_laddari`, `mrk_ladsehingga`, `mrk_ladsehari`) VALUES
-(1, 'dwefdwef', '', 'wefwef', 'fwefwef', '', '0000-00-00', '0000-00-00', '0000-00-00', '0000-00-00', '0', '0', '0000-00-00', '0000-00-00', '0');
+INSERT INTO `mrk_tiga` (`id`, `mrk_nopkk`, `mrk_namakon`, `mrk_nokontrak`, `mrk_noinden`, `mrk_tajukkerja`, `mrk_tarikhmulakon`, `mrk_tarikhjangkasiap`, `mrk_lanjutmasa`, `mrk_siapsebenar`, `mrk_kosprojek`, `mrk_kossebenar`, `mrk_laddari`, `mrk_ladsehingga`, `mrk_ladsehari`, `mrktiga_id`) VALUES
+(1, 'dwefdwef', '', 'wefwef', 'fwefwef', '', '0000-00-00', '0000-00-00', '0000-00-00', '0000-00-00', '0', '0', '0000-00-00', '0000-00-00', '0', 0);
 
 --
 -- Indexes for dumped tables
@@ -529,7 +540,7 @@ ALTER TABLE `dp_gps`
 -- AUTO_INCREMENT for table `dp_projek`
 --
 ALTER TABLE `dp_projek`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `dp_projekinfo`
@@ -571,7 +582,7 @@ ALTER TABLE `mrk_perakuansiapbaikicacat`
 -- AUTO_INCREMENT for table `mrk_satu`
 --
 ALTER TABLE `mrk_satu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `mrk_suratkhas`
