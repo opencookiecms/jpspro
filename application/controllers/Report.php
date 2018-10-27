588914fb99665dcf5d96a271cbc3eb48138d3441
opencookiecms <syed.m.afiq@outlook.com>
2018-10-28 03:08:49 +0800

    delete

--- application/controllers/Report.php
+++ application/controllers/Report.php
@@ -3,13 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Report extends CI_Controller{
 
-  public function __construct()
-  {
-    parent::__construct();
-    $this->load->library('Report_pdf');
-  }
-
-  function index()
+  function Report_MRK_01()
   {
 
     $this->load->view('print/mrk01_report');
