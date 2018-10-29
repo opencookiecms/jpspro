b60cb6b22f20934c07d12acd3df7d74c9c8b52d3
Alyya Izzati <33283754+alyyaizzati@users.noreply.github.com>
2018-10-29 12:00:00 +0800

    Update application/controllers/MRK.php

--- application/controllers/MRK.php
+++ application/controllers/MRK.php
@@ -115,7 +115,7 @@ class MRK extends CI_Controller{
 		{
 			$this->load->view('template/header');
 			$this->load->view('template/sidebar');
-			$this->load->view('pages/LSK',$data);
+			$this->load->view('pages/MRK03',$data);
 			$this->load->view('template/footer');
 		}
 		else
