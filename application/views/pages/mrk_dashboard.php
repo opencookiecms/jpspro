628a5c30511768fd9d08bdb084e7178c861d9605
opencookiecms <syed.m.afiq@outlook.com>
2019-01-21 22:33:12 +0800

    Update mrk_dashboard.php

--- application/views/pages/mrk_dashboard.php
+++ application/views/pages/mrk_dashboard.php
@@ -1,101 +1,314 @@
 
-      <div class="main-panel">
-        <div class="content-wrapper cnt" style="background: #C9D6FF;background: -webkit-linear-gradient(to right, #E2E2E2, #C9D6FF);background: linear-gradient(to right, #E2E2E2, #C9D6FF);">
-          <div class="row purchace-popup">
-            <div class="col-12">
-              <span class="d-block d-md-flex align-items-center">
-                <p>Surat-Menyurat.</p>
-              </span>
+<div class="main-panel">
+  <div class="content-wrapper cnt" style="background: #C9D6FF;background: -webkit-linear-gradient(to right, #E2E2E2, #C9D6FF);background: linear-gradient(to right, #E2E2E2, #C9D6FF);">
+    <div class="row purchace-popup">
+      <div class="col-12">
+        <span class="d-block d-md-flex align-items-center">
+          <p>Surat-Menyurat.</p>
+        </span>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 grid-margin stretch-card">
+        <div class="card card-statistics" style="border-radius:10px;">
+          <div class="card-body">
+            <div class="clearfix">
+              <div class="float-left">
+                <i class="mdi mdi-cube text-primary icon-lg"></i>
+              </div>
+              <div class="float-right">
+                <p class="mb-0 text-right">Dokumen</p>
+                <div class="fluid-container">
+                  <h3 class="font-weight-medium text-right mb-0">MRK-01</h3>
+                </div>
+              </div>
             </div>
+            <p class="text-muted mt-3 mb-0">
+              <i class="mdi mdi-bookmark-outline mr-1" aria-hidden="true"></i>Maklumat Rekod Kerja 01</i></a>
+            </p>
+            <p class="text-muted mt-3 mb-0">
+              <i class="mdi mdi-bookmark-outline mr-1" aria-hidden="true"></i><a href="#" class="small-box-footer">Masuk <i class="fa fa-arrow-circle-right"></i></a>
+            </p>
           </div>
-          <div class="row">
-            <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 grid-margin stretch-card">
-              <div class="card card-statistics" style="border-radius:10px;">
-                <div class="card-body">
-                  <div class="clearfix">
-                    <div class="float-left">
-                      <i class="mdi mdi-cube text-primary icon-lg"></i>
-                    </div>
-                    <div class="float-right">
-                      <p class="mb-0 text-right">Dokumen</p>
-                      <div class="fluid-container">
-                        <h3 class="font-weight-medium text-right mb-0">MRK-01</h3>
-                      </div>
-                    </div>
-                  </div>
-                  <p class="text-muted mt-3 mb-0">
-                    <i class="mdi mdi-alert-octagon mr-1" aria-hidden="true"></i><a href="#" class="small-box-footer">Masuk <i class="fa fa-arrow-circle-right"></i></a>
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 grid-margin stretch-card">
-              <div class="card card-statistics" style="border-radius:10px;">
-                <div class="card-body">
-                  <div class="clearfix">
-                    <div class="float-left">
-                      <i class="mdi mdi-cube text-warning icon-lg"></i>
-                    </div>
-                    <div class="float-right">
-                      <p class="mb-0 text-right">Dokuemen</p>
-                      <div class="fluid-container">
-                        <h3 class="font-weight-medium text-right mb-0">MRK-02</h3>
-                      </div>
-                    </div>
-                  </div>
-                  <p class="text-muted mt-3 mb-0">
-                    <i class="mdi mdi-bookmark-outline mr-1" aria-hidden="true"></i><a href="#" class="small-box-footer">Masuk <i class="fa fa-arrow-circle-right"></i></a>
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 grid-margin stretch-card">
-              <div class="card card-statistics" style="border-radius:10px;">
-                <div class="card-body">
-                  <div class="clearfix">
-                    <div class="float-left">
-                      <i class="mdi mdi-cube text-success icon-lg"></i>
-                    </div>
-                    <div class="float-right">
-                      <p class="mb-0 text-right">Dokumen</p>
-                      <div class="fluid-container">
-                        <h3 class="font-weight-medium text-right mb-0">LSK</h3>
-                      </div>
-                    </div>
-                  </div>
-
-                  <p class="text-muted mt-3 mb-0">
-                    <i class="mdi mdi-bookmark-outline mr-1" aria-hidden="true"></i>Laporan Siap Kerja</i></a>
-                  </p>
-                  <p class="text-muted mt-3 mb-0">
-                    <i class="mdi mdi-bookmark-outline mr-1" aria-hidden="true"></i><a href="#" class="small-box-footer">Masuk <i class="fa fa-arrow-circle-right"></i></a>
-                  </p>
-                </div>
-              </div>
-            </div>
-
-            <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 grid-margin stretch-card">
-              <div class="card card-statistics" style="border-radius:10px;">
-                <div class="card-body">
-                  <div class="clearfix">
-                    <div class="float-left">
-                      <i class="mdi mdi-cube text-info icon-lg"></i>
-                    </div>
-                    <div class="float-right">
-                      <p class="mb-0 text-right">Dokumen</p>
-                      <div class="fluid-container">
-                        <h3 class="font-weight-medium text-right mb-0">MRK-03</h3>
-                      </div>
-                    </div>
-                  </div>
-                  <p class="text-muted mt-3 mb-0">
-                    <i class="mdi mdi-bookmark-outline mr-1" aria-hidden="true"></i><a href="#" class="small-box-footer">Masuk <i class="fa fa-arrow-circle-right"></i></a>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-
         </div>
-        <!-- content-wrapper ends -->
-        <!-- partial:partials/_footer.html -->
+      </div>
+      <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 grid-margin stretch-card">
+        <div class="card card-statistics" style="border-radius:10px;">
+          <div class="card-body">
+            <div class="clearfix">
+              <div class="float-left">
+                <i class="mdi mdi-cube text-warning icon-lg"></i>
+              </div>
+              <div class="float-right">
+                <p class="mb-0 text-right">Dokuemen</p>
+                <div class="fluid-container">
+                  <h3 class="font-weight-medium text-right mb-0">MRK-02</h3>
+                </div>
+              </div>
+            </div>
+            <p class="text-muted mt-3 mb-0">
+              <i class="mdi mdi-bookmark-outline mr-1" aria-hidden="true"></i>Maklumat Rekod Kerja 02</i></a>
+            </p>
+            <p class="text-muted mt-3 mb-0">
+              <i class="mdi mdi-bookmark-outline mr-1" aria-hidden="true"></i><a href="#" class="small-box-footer">Masuk <i class="fa fa-arrow-circle-right"></i></a>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 grid-margin stretch-card">
+        <div class="card card-statistics" style="border-radius:10px;">
+          <div class="card-body">
+            <div class="clearfix">
+              <div class="float-left">
+                <i class="mdi mdi-folder-multiple-outline text-success icon-lg"></i>
+              </div>
+              <div class="float-right">
+                <p class="mb-0 text-right">Dokumen</p>
+                <div class="fluid-container">
+                  <h3 class="font-weight-medium text-right mb-0">LSK</h3>
+                </div>
+              </div>
+            </div>
+
+            <p class="text-muted mt-3 mb-0">
+              <i class="mdi mdi-bookmark-outline mr-1" aria-hidden="true"></i>Laporan Siap Kerja</i></a>
+            </p>
+            <p class="text-muted mt-3 mb-0">
+              <i class="mdi mdi-bookmark-outline mr-1" aria-hidden="true"></i><a href="#" class="small-box-footer">Masuk <i class="fa fa-arrow-circle-right"></i></a>
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 grid-margin stretch-card">
+        <div class="card card-statistics" style="border-radius:10px;">
+          <div class="card-body">
+            <div class="clearfix">
+              <div class="float-left">
+                <i class="mdi mdi-cube text-info icon-lg"></i>
+              </div>
+              <div class="float-right">
+                <p class="mb-0 text-right">Dokumen</p>
+                <div class="fluid-container">
+                  <h3 class="font-weight-medium text-right mb-0">MRK-03</h3>
+                </div>
+              </div>
+            </div>
+            <p class="text-muted mt-3 mb-0">
+              <i class="mdi mdi-bookmark-outline mr-1" aria-hidden="true"></i>Maklumat Rekod Kerja 03</i></a>
+            </p>
+            <p class="text-muted mt-3 mb-0">
+              <i class="mdi mdi-bookmark-outline mr-1" aria-hidden="true"></i><a href="#" class="small-box-footer">Masuk <i class="fa fa-arrow-circle-right"></i></a>
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 grid-margin stretch-card">
+        <div class="card card-statistics" style="border-radius:10px;">
+          <div class="card-body">
+            <div class="clearfix">
+              <div class="float-left">
+                <i class="mdi mdi-newspaper text-danger icon-lg"></i>
+              </div>
+              <div class="float-right">
+                <p class="mb-0 text-right">Dokumen</p>
+                <div class="fluid-container">
+                  <h3 class="font-weight-medium text-right mb-0">PSK</h3>
+                </div>
+              </div>
+            </div>
+
+            <p class="text-muted mt-3 mb-0">
+              <i class="mdi mdi-bookmark-outline mr-1" aria-hidden="true"></i>Perakuan Siap Kerja</i></a>
+            </p>
+            <p class="text-muted mt-3 mb-0">
+              <i class="mdi mdi-bookmark-outline mr-1" aria-hidden="true"></i><a href="#" class="small-box-footer">Masuk <i class="fa fa-arrow-circle-right"></i></a>
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 grid-margin stretch-card">
+        <div class="card card-statistics" style="border-radius:10px;">
+          <div class="card-body">
+            <div class="clearfix">
+              <div class="float-left">
+                <i class="mdi  mdi-check-all text-primary icon-lg"></i>
+              </div>
+              <div class="float-right">
+                <p class="mb-0 text-right">Dokumen</p>
+                <div class="fluid-container">
+                  <h3 class="font-weight-medium text-right mb-0">SS</h3>
+                </div>
+              </div>
+            </div>
+
+            <p class="text-muted mt-3 mb-0">
+              <i class="mdi mdi-bookmark-outline mr-1" aria-hidden="true"></i>Senarai Semakan</i></a>
+            </p>
+            <p class="text-muted mt-3 mb-0">
+              <i class="mdi mdi-bookmark-outline mr-1" aria-hidden="true"></i><a href="#" class="small-box-footer">Masuk <i class="fa fa-arrow-circle-right"></i></a>
+            </p>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 grid-margin stretch-card">
+        <div class="card card-statistics" style="border-radius:10px;">
+          <div class="card-body">
+            <div class="clearfix">
+              <div class="float-left">
+                <i class="mdi mdi-ambulance text-info icon-lg"></i>
+              </div>
+              <div class="float-right">
+                <p class="mb-0 text-right">Dokumen</p>
+                <div class="fluid-container">
+                  <h3 class="font-weight-medium text-right mb-0">PSMK</h3>
+                </div>
+              </div>
+            </div>
+
+            <p class="text-muted mt-3 mb-0">
+              <i class="mdi mdi-bookmark-outline mr-1" aria-hidden="true"></i>Perakuan Siap Membaiki Kecatatan</i></a>
+            </p>
+            <p class="text-muted mt-3 mb-0">
+              <i class="mdi mdi-bookmark-outline mr-1" aria-hidden="true"></i><a href="#" class="small-box-footer">Masuk <i class="fa fa-arrow-circle-right"></i></a>
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 grid-margin stretch-card">
+        <div class="card card-statistics" style="border-radius:10px;">
+          <div class="card-body">
+            <div class="clearfix">
+              <div class="float-left">
+                <i class="mdi  mdi-bank text-warning icon-lg"></i>
+              </div>
+              <div class="float-right">
+                <p class="mb-0 text-right">Dokumen</p>
+                <div class="fluid-container">
+                  <h3 class="font-weight-medium text-right mb-0">PJB</h3>
+                </div>
+              </div>
+            </div>
+
+            <p class="text-muted mt-3 mb-0">
+              <i class="mdi mdi-bookmark-outline mr-1" aria-hidden="true"></i>Pelepasan Jaminan Bank</i></a>
+            </p>
+            <p class="text-muted mt-3 mb-0">
+              <i class="mdi mdi-bookmark-outline mr-1" aria-hidden="true"></i><a href="#" class="small-box-footer">Masuk <i class="fa fa-arrow-circle-right"></i></a>
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 grid-margin stretch-card">
+        <div class="card card-statistics" style="border-radius:10px;">
+          <div class="card-body">
+            <div class="clearfix">
+              <div class="float-left">
+                <i class="mdi mdi-tag text-success icon-lg"></i>
+              </div>
+              <div class="float-right">
+                <p class="mb-0 text-right">Dokumen</p>
+                <div class="fluid-container">
+                  <h3 class="font-weight-medium text-right mb-0">PP WJP</h3>
+                </div>
+              </div>
+            </div>
+
+            <p class="text-muted mt-3 mb-0">
+              <i class="mdi mdi-bookmark-outline mr-1" aria-hidden="true">Perakuan Pemulangan WJP</i></i></a>
+            </p>
+            <p class="text-muted mt-3 mb-0">
+              <i class="mdi mdi-bookmark-outline mr-1" aria-hidden="true"></i><a href="#" class="small-box-footer">Masuk <i class="fa fa-arrow-circle-right"></i></a>
+            </p>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 grid-margin stretch-card">
+        <div class="card card-statistics" style="border-radius:10px;">
+          <div class="card-body">
+            <div class="clearfix">
+              <div class="float-left">
+                <i class="mdi mdi-library-books text-danger icon-lg"></i>
+              </div>
+              <div class="float-right">
+                <p class="mb-0 text-right">Dokumen</p>
+                <div class="fluid-container">
+                  <h3 class="font-weight-medium text-right mb-0">MRK</h3>
+                </div>
+              </div>
+            </div>
+
+            <p class="text-muted mt-3 mb-0">
+              <i class="mdi mdi-bookmark-outline mr-1" aria-hidden="true">Surat MRK</i></i></a>
+            </p>
+            <p class="text-muted mt-3 mb-0">
+              <i class="mdi mdi-bookmark-outline mr-1" aria-hidden="true"></i><a href="#" class="small-box-footer">Masuk <i class="fa fa-arrow-circle-right"></i></a>
+            </p>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 grid-margin stretch-card">
+        <div class="card card-statistics" style="border-radius:10px;">
+          <div class="card-body">
+            <div class="clearfix">
+              <div class="float-left">
+                <i class="mdi mdi-image-filter-frames text-success icon-lg"></i>
+              </div>
+              <div class="float-right">
+                <p class="mb-0 text-right">Dokumen</p>
+                <div class="fluid-container">
+                  <h3 class="font-weight-medium text-right mb-0">S.Khas</h3>
+                </div>
+              </div>
+            </div>
+
+            <p class="text-muted mt-3 mb-0">
+              <i class="mdi mdi-bookmark-outline mr-1" aria-hidden="true">Surat Khas</i></i></a>
+            </p>
+            <p class="text-muted mt-3 mb-0">
+              <i class="mdi mdi-bookmark-outline mr-1" aria-hidden="true"></i><a href="#" class="small-box-footer">Masuk <i class="fa fa-arrow-circle-right"></i></a>
+            </p>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 grid-margin stretch-card">
+        <div class="card card-statistics" style="border-radius:10px;">
+          <div class="card-body">
+            <div class="clearfix">
+              <div class="float-left">
+                <i class="mdi mdi-memory text-info icon-lg"></i>
+              </div>
+              <div class="float-right">
+                <p class="mb-0 text-right">Dokumen</p>
+                <div class="fluid-container">
+                  <h3 class="font-weight-medium text-right mb-0">Surat</h3>
+                </div>
+              </div>
+            </div>
+
+            <p class="text-muted mt-3 mb-0">
+              <i class="mdi mdi-bookmark-outline mr-1" aria-hidden="true">Surat Pelepasan WJP</i></i></a>
+            </p>
+            <p class="text-muted mt-3 mb-0">
+              <i class="mdi mdi-bookmark-outline mr-1" aria-hidden="true"></i><a href="#" class="small-box-footer">Masuk <i class="fa fa-arrow-circle-right"></i></a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+  <!-- content-wrapper ends -->
+  <!-- partial:partials/_footer.html -->
