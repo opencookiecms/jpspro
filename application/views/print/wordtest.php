bb0a7298c768f75e2f42b0f9f08075209338f025
opencookiecms <syed.m.afiq@outlook.com>
2019-03-05 19:56:25 +0800

    s

--- application/views/print/wordtest.php
+++ application/views/print/wordtest.php
@@ -1,5 +1,14 @@
 <?php
-$templateProcessor = new \PhpOffice\PhpWord\TemplateProcessor(__DIR__ ."\stest.docx");
 
-$templateProcessor->setValue('Name', 'John Doe');
-$templateProcessor->setValue(array('City', 'Street'), array('Detroit', '12th Street'));
+echo __FILE__;
+//$templateProcessor = new \PhpOffice\PhpWord\TemplateProcessor(__DIR__ ."\stest.docx");
+
+//$templateProcessor->setValue('Name', 'John Doe');
+
+
+//$templateProcessor->saveAs('MRK01.docx');
+
+
+//header("Content-Type:application/msword");
+//header("Content-Disposition: attachment;filename=SSS.docx");
+//readfile('MRK01.docx');
