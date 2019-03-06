da5a239b5c968c162eeb858419396115619800c1
opencookiecms <syed.m.afiq@outlook.com>
2019-03-06 15:06:53 +0800

    remove

--- application/libraries/vendor/composer/autoload_namespaces.php
+++ application/libraries/vendor/composer/autoload_namespaces.php
@@ -6,6 +6,4 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'PhantomInstaller\\' => array($vendorDir . '/jakoch/phantomjs-installer/src'),
-    'Icecave\\Parity' => array($vendorDir . '/icecave/parity/src'),
 );
--- application/views/print/wordtest.php
+++ application/views/print/wordtest.php
@@ -1,14 +1,14 @@
 <?php
 
-echo __FILE__;
-//$templateProcessor = new \PhpOffice\PhpWord\TemplateProcessor(__DIR__ ."\stest.docx");
+//echo __FILE__;
+$templateProcessor = new \PhpOffice\PhpWord\TemplateProcessor(__DIR__ ."\stest.docx");
 
-//$templateProcessor->setValue('Name', 'John Doe');
-
-
-//$templateProcessor->saveAs('MRK01.docx');
+$templateProcessor->setValue('Name', 'John Doe');
 
 
-//header("Content-Type:application/msword");
-//header("Content-Disposition: attachment;filename=SSS.docx");
-//readfile('MRK01.docx');
+$templateProcessor->saveAs('MRK01.docx');
+
+
+header("Content-Type:application/msword");
+header("Content-Disposition: attachment;filename=SSS.docx");
+readfile('MRK01.docx');
