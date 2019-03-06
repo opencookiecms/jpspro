f59c06c33443882415554dbb7ac32ea03fd8cefb
opencookiecms <syed.m.afiq@outlook.com>
2019-03-06 15:07:36 +0800

    i

--- application/views/pages/Landing.php
+++ application/views/pages/Landing.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+xc<!DOCTYPE html>
 <html lang="en">
 
 <head>
--- application/views/print/wordtest.php
+++ application/views/print/wordtest.php
@@ -1,14 +1,15 @@
 <?php
 
 echo __FILE__;
-//$templateProcessor = new \PhpOffice\PhpWord\TemplateProcessor(__DIR__ ."\stest.docx");
+$templateProcessor = new \PhpOffice\PhpWord\TemplateProcessor(__DIR__ ."\stest.docx");
 
-//$templateProcessor->setValue('Name', 'John Doe');
+$templateProcessor->setValue('Name', 'SyedMAfiq');
 
 
-//$templateProcessor->saveAs('MRK01.docx');
+$templateProcessor->saveAs('MRK01.docx');
 
 
 //header("Content-Type:application/msword");
-//header("Content-Disposition: attachment;filename=SSS.docx");
+//header("Content-Disposition: attachment;filename=MRK01.docx");
 //readfile('MRK01.docx');
+$success = Gears\Pdf::convert(__DIR__ ."\stest.docx",__DIR__ ."\stest.pdf");
