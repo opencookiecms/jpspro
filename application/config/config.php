dc9bb42b378b058e605e14e08443703456cd235a
SYED MOHD AFIQ SYED ABDUL RAHMAN <syed.m.afiq@outlook.com>
2019-10-10 00:04:12 +0800

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -24,8 +24,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-//$config['base_url'] = 'http://localhost/jpspro';
-$config['base_url'] = 'http://www.jpskmsb.gov.my/jpspro';
+$config['base_url'] = 'http://192.168.64.2/jpspro';
+//$config['base_url'] = 'http://www.jpskmsb.gov.my/jpspro';
 
 /*
 |--------------------------------------------------------------------------
