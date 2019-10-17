1c20dbc0545472af7d4fa1d0fc7b0bb289c18361
opencookiecms <syed.m.afiq@outlook.com>
2019-10-17 11:19:07 +0800

    Update Setting_model.php

--- application/models/Setting_model.php
+++ application/models/Setting_model.php
@@ -72,15 +72,18 @@ class Setting_model extends CI_Model{
          $this->db->from('dp_projek');
          $this->db->join('dp_projekinfo', 'dp_projekinfo.dp_id = dp_projek.projek_id','right');
          $this->db->where('dp_projekinfo.df_penolong',$user);
+         $query = $this->db->get();
+         return $query->result();
     }
     else
     {
          $this->db->select("projek_id, COUNT(*) AS TOTAL, COUNT(IF(df_jsebutharga='Lantikan Terus',1,null)) AS lt, COUNT(IF(df_jsebutharga='Undi',1,null)) AS undi, COUNT(IF(df_jsebutharga='Sebutharga',1,null)) AS sb");
          $this->db->from('dp_projek');
+          $query = $this->db->get();
+          return $query->result();
     }
   
-    $query = $this->db->get();
-    return $query->result();
+
 
   }
 
